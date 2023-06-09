6dad87ee1ab7d8858baf18740c3fbbccbbd9fdae
Kurt Ostfeld <kurt@samba.tv>
2023-06-08 23:57:49 -0500

    updates

--- flink-kryo-upgraded/src/main/java/demo/app/Main.java
+++ flink-kryo-upgraded/src/main/java/demo/app/Main.java
@@ -13,24 +13,20 @@ import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.api.connector.sink2.SinkWriter;
 import org.apache.flink.api.connector.source.lib.NumberSequenceSource;
 import org.apache.flink.api.java.typeutils.GenericTypeInfo;
-import org.apache.flink.client.cli.CliFrontend;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.configuration.DeploymentOptions;
-import org.apache.flink.runtime.jobgraph.RestoreMode;
-import org.apache.flink.runtime.jobgraph.SavepointConfigOptions;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-//    public final static String KRYO_V2_NATIVE_SAVEPOINT_PATH = "../native-savepoints/savepoint-0bed3c-7f053b08f084";
-//    public final static String KRYO_V5_NATIVE_SAVEPOINT_PATH = "../native-savepoints/savepoint-51d0d3-da9ea6535060";
-
     @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -39,13 +35,20 @@ public class Main {
                 logger.info("Starting up.");
 
                 final Configuration flinkConfiguration = new Configuration();
-//                flinkConfiguration.set(DeploymentOptions.ATTACHED, false);
-                // Uncomment to debug restoring from savepoint.
-//                flinkConfiguration.set(SavepointConfigOptions.SAVEPOINT_PATH, KRYO_V5_NATIVE_SAVEPOINT_PATH);
-//                flinkConfiguration.set(SavepointConfigOptions.SAVEPOINT_IGNORE_UNCLAIMED_STATE, false);
-//                flinkConfiguration.set(SavepointConfigOptions.RESTORE_MODE, RestoreMode.NO_CLAIM);
-//                logger.info("SAVEPOINT_PATH={}", flinkConfiguration.get(SavepointConfigOptions.SAVEPOINT_PATH));
+                flinkConfiguration.set(ExecutionCheckpointingOptions.ENABLE_CHECKPOINTS_AFTER_TASKS_FINISH, true);
+
                 final StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment(flinkConfiguration);
+                streamEnv.enableCheckpointing(1000);
+                streamEnv.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+                streamEnv.getCheckpointConfig().setMinPauseBetweenCheckpoints(500);
+                streamEnv.getCheckpointConfig().setCheckpointTimeout(60000);
+                streamEnv.getCheckpointConfig().setTolerableCheckpointFailureNumber(2);
+                streamEnv.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+                streamEnv.getCheckpointConfig().setExternalizedCheckpointCleanup(
+                        CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+                streamEnv.getCheckpointConfig().enableUnalignedCheckpoints();
+                streamEnv.getCheckpointConfig().setCheckpointStorage("file:///Users/kurt/opt/flink/checkpoints");
+
                 final ExecutionConfig executionConfig = streamEnv.getConfig();
                 executionConfig.setRestartStrategy(new RestartStrategies.NoRestartStrategyConfiguration());
                 streamEnv.setParallelism(1);
